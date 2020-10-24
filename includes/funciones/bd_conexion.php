748db48d08e5262c720ee8219a44b4baa8e31a35
Luis Zunino <zuninoluis01@gmail.com>
2020-10-24 02:17:32 -0300

    Update bd_conexion.php
    se quito la contraseña root para que cargara la base de datos

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,7 +1,7 @@
 <?php
     define('GDLWEBCAMP_HOST', 'localhost');
     define('GDLWEBCAMP_DB_USUARIO', 'root');
-    define('GDLWEBCAMP_DB_PASSWORD', 'root');
+    define('GDLWEBCAMP_DB_PASSWORD', '');
     define('GDLWEBCAMP_DB_DATABASE', 'gdlwebcamp');
 
     $conn = new mysqli(GDLWEBCAMP_HOST, GDLWEBCAMP_DB_USUARIO, GDLWEBCAMP_DB_PASSWORD, GDLWEBCAMP_DB_DATABASE);
