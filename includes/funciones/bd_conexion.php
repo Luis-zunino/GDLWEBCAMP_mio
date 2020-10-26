693fd97c335ed46bb43968e548ea12b07f2cc940
Luis Zunino <zuninoluis01@gmail.com>
2020-10-26 17:35:52 -0300

    Update bd_conexion.php
    se crea nueva base de datos en phpmyadmin y se actualiza archivo de conexion para que tome la nueva bd

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,10 +1,10 @@
 <?php
-    define('GDLWEBCAMP_HOST', 'localhost');
-    define('GDLWEBCAMP_DB_USUARIO', 'root');
-    define('GDLWEBCAMP_DB_PASSWORD', '');
-    define('GDLWEBCAMP_DB_DATABASE', 'gdlwebcamp');
+    define('GDLWEBCAMP1_HOST', 'localhost');
+    define('GDLWEBCAMP1_DB_USUARIO', 'root');
+    define('GDLWEBCAMP1_DB_PASSWORD', '');
+    define('GDLWEBCAMP1_DB_DATABASE', 'gdlwebcamp1');
 
-    $conn = new mysqli(GDLWEBCAMP_HOST, GDLWEBCAMP_DB_USUARIO, GDLWEBCAMP_DB_PASSWORD, GDLWEBCAMP_DB_DATABASE);
+    $conn = new mysqli(GDLWEBCAMP1_HOST, GDLWEBCAMP1_DB_USUARIO, GDLWEBCAMP1_DB_PASSWORD, GDLWEBCAMP1_DB_DATABASE);
 
     if($conn->connect_error) {
       echo $conn->connect_error;
