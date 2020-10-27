83091c56cdc2747a0f89b8a3e00a1eeb12f12ead
Luis Zunino <zuninoluis01@gmail.com>
2020-10-26 22:09:38 -0300

    Update validar_registro.php
    se completa validar registro

--- validar_registro.php
+++ validar_registro.php
@@ -1,5 +1,6 @@
 <?php include_once "includes/templates/header.php"; ?>
-
+<section class="seccion contenedor">
+    <h2>Resumen Registro</h2>
 <pre>
     <?php var_dump($_POST) ?>
 </pre>
@@ -10,6 +11,6 @@
 
 
 
-
+</section>
 
 <?php include_once "includes/templates/footer.php"; ?>
